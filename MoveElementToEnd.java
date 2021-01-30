108c273d1272d60d7b7b1ece4c986df60dec2533
SumedhaI <sumedhainamdar1@gmail.com>
2021-01-30 11:09:40 -0500

    Create MoveElementToEnd.java
    Time complexity- n (log n)

---
+++ MoveElementToEnd.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class MoveElementToEnd {
+  public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
+    Collections.sort(array);
+		int end = array.size()-1;
+		int i = 0;
+		while(i<end){
+			if(array.get(i)==toMove){
+				array.set(i, array.get(end));
+				array.set(end, toMove);
+				end--;
+			}
+			else
+				i++;
+		}
+		
+    return array;
+  }
+}
